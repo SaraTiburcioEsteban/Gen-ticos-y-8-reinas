969f92b0570096591026c3f856b1e2bcba0b9ac5
SaraTiburcioEsteban <zs20016130@estidiantes.uv.mx>
2023-10-09 15:38:38 -0600

    se agregó el archivo main y nreinas que contienen las notas de mi proyecto

---
+++ nreinas.py
@@ -0,0 +1,11 @@
+import random
+#matriz simple de 0 a 8
+s=(range(8) )
+
+random.shuffle(s)
+print(s)
+#final
+for i in range[n]:
+    solucion.append(0)
+    etapa: int = 0
+print(Reinas(solucion, etapa,n))
